d125753ab65c9547d02b01bc4a9e9593dae48413
IamDucloux <T-101@PC224>
2018-11-29 19:08:30 -0600

    Peligro

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -7,6 +7,8 @@ package holamundo;
 
 import java.awt.Point;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 /**
@@ -20,17 +22,33 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JFrame ventanita = new JFrame("¡VIRUS!");
-        ventanita.setSize(300,300);
-        ventanita.setVisible(true);
-        ventanita.setLocationRelativeTo(null);
-        Point punto=ventanita.getLocationOnScreen();
-        Random x = new Random(punto.x);
-        Random y = new Random(punto.y);
-        int dx = x.nextInt()*2;
-        int dy = y.nextInt()*2;
-        ventanita.setLocation(dx, dy);
-        
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                        JFrame ventanita = new JFrame("¡VIRUS!");
+                        ventanita.setSize(300, 300);
+                        ventanita.setVisible(true);
+                        ventanita.setLocationRelativeTo(null);
+                        Point p = ventanita.getLocationOnScreen();
+                        int largo = p.x * 2;
+                        int ancho = p.y * 2;
+                        Random r1 = new Random();
+                        Random r2 = new Random();
+                        int dx = r1.nextInt(largo);
+                        int dy = r2.nextInt(ancho);
+                        ventanita.setLocation(dx, dy);
+                    } catch (InterruptedException ex) {
+
+                    }
+
+                }
+            }
+        }
+        );
+        t1.start();
     }
-    
+
 }
